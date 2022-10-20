2719772d8c15bf23654ba8dff8c06e4c8d00ee98
umerkang66 <umerkang66@gmail.com>
2022-10-20 04:25:44 +0000

    add db utils

---
+++ utils/db-connect.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const dbConnect = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    // if the connection connected, connecting, just return
+    return;
+  }
+  // only if the connection is disconnected, run this line
+  await mongoose.connect(process.env.DB_URI!);
+  console.log("Connected to DB");
+};
